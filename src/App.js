fc61b9d8790fd7c9f0eb08da5646dad41699543a
YyamanSharma <sharmayaman1996@gmail.com>
2021-03-20 15:20:03 +0530

    Added Collection Item, Collection Preview and ShopPage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
+import React from 'react';
 import './App.css';
-import {Route,Switch} from 'react-router-dom'
-import HomePage from './pages/homepage/homepage.component'
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const HatsPage=()=><div>HatsPage</div>
-
-const App =()=> {
+const App = () => {
   return (
-    <div >
+    <div>
       <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatsPage}/>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
